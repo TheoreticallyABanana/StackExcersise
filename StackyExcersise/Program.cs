edbed1c40cdff393ea5dbaf4f20c53a69e159616
Jeffrey Lee <J220788@tafe.wa.edu.au>
2017-07-17 14:40:06 +0800

    added new excersise stacky

---
+++ StackyExcersise/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackyMethods;
+
+
+namespace StackyExcersise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calling referenced stack methods
+            methods stackyExtreme = new methods(10);
+
+            //Pushing items into array
+            stackyExtreme.push("a");
+            stackyExtreme.push("b");
+            stackyExtreme.push("c");
+            stackyExtreme.push("d");
+
+            //print contents of array
+            Console.WriteLine(stackyExtreme.peek());
+
+            //Removing top item from stack
+            stackyExtreme.pop();
+            Console.WriteLine(stackyExtreme.peek());
+
+            //Filling stack
+            stackyExtreme.push("e");
+            stackyExtreme.push("f");
+            stackyExtreme.push("g");
+            stackyExtreme.push("h");
+            stackyExtreme.push("i");
+            stackyExtreme.push("j");
+
+            //checking if the stack is full
+            if (stackyExtreme.isFull())
+            {
+                Console.WriteLine("STACKY IS FULL");
+            }
+
+            //Removing stack contents 
+            for (int i = 0; i < 10; i++)
+            {
+                stackyExtreme.pop();
+            }
+
+            //checking if the stack is empty
+            if (stackyExtreme.isEmpty())
+            {
+                Console.WriteLine("STACKY IS EMPTY");
+            }
+
+
+
+            
+        }
+    }
+}
---
+++ StackyMethods/methods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackyMethods
+{
+    public class methods
+    {
+        string[] stackArray;
+        int top;
+        int maxSize = 0;
+
+        public methods(int elementSize)
+        {
+            maxSize = elementSize;
+            stackArray = new String[maxSize];
+        }
+
+        public void push(string j) // put item on top of stack
+        {
+            stackArray[++top] = j; // increment top, insert item
+        }
+        public string pop() // take item from top of stack
+        {
+            return stackArray[top--]; // access item, decrement top
+        }
+        public string peek() // peek at top of stack
+        {
+            return stackArray[top];
+        }
+        public bool isEmpty() // true if stack is empty
+        {
+            return (top == -1);
+        }
+        public bool isFull() // true if stack is full
+        {
+            return (top == maxSize - 1);
+        }
+    }
+}
